941a4e1866d7593fbfc73a0a326cb72470b11b7b
Levi-1103 <58707841+Levi-1103@users.noreply.github.com>
2025-07-03 16:09:24 +0100

    Init fontend and backend setup

---
+++ frontend/src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count: number = $state(0)
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick={increment}>
+  count is {count}
+</button>
